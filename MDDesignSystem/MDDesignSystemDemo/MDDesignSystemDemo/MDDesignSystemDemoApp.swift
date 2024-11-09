8b83fb560413bdd64ecf23af61d42c6489db52c4
Małgorzata Dziubich <malgorzata.dziubich@gmail.com>
2024-11-09 14:21:24 +0100

    Add demo app

---
+++ MDDesignSystem/MDDesignSystemDemo/MDDesignSystemDemo/MDDesignSystemDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDDesignSystemDemoApp.swift
+//  MDDesignSystemDemo
+//
+//  Created by Małgorzata Dziubich on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MDDesignSystemDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
