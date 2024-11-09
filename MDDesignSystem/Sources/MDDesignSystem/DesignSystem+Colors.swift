5965cd86c51c5d9eeb88bcb924173514ee2ec7f3
Małgorzata Dziubich <malgorzata.dziubich@gmail.com>
2024-11-09 13:24:58 +0100

    Prepase base for design system

---
+++ MDDesignSystem/Sources/MDDesignSystem/DesignSystem+Colors.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+extension DesignSystem {
+
+    // This code was inspired by the article:
+    // https://atlassian.design/foundations/color
+    enum Color: String {
+
+        // MARK: - Content
+
+        case contentPrimary
+        case contentSecondary
+        case contentDisabled
+        case contentBorder
+        case contentAccent
+
+        // MARK: - Background
+
+        case backgroundPrimary
+        case backgroundSecondary
+
+        var value: SwiftUI.Color {
+            SwiftUI.Color(self.rawValue)
+        }
+    }
+}
